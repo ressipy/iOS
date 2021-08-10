60dfa33a916ed41d4c9038b431d81ba33564f2b2
Dennis Beatty <dennis@dcbeatty.com>
2021-08-10 12:07:42 -0600

    Beautify the account screen

--- Ressipy/Screens/Account/AccountView.swift
+++ Ressipy/Screens/Account/AccountView.swift
@@ -13,13 +13,29 @@ struct AccountView: View {
     var body: some View {
         VStack {
             if vm.isLoggedIn {
-                Text("You're logged in as \(vm.userEmail!)")
+                Text("You're logged in as")
+                    .font(.subheadline)
+                    .padding(.top, 180)
+                
+                Text(vm.userEmail!)
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .padding(.top, 2)
+                
+                Spacer()
                 
                 Button {
                     vm.signOut()
                 } label: {
                     Text("Sign out")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .frame(width: 260, height: 50)
+                        .foregroundColor(.white)
+                        .background(Color.red)
+                        .cornerRadius(10)
                 }
+                .padding(.bottom, 64)
             } else {
                 LoginView()
             }
@@ -29,6 +45,13 @@ struct AccountView: View {
 
 struct AccountView_Previews: PreviewProvider {
     static var previews: some View {
-        AccountView()
+        AccountView(vm: createViewModel())
+    }
+    
+    static func createViewModel() -> AccountViewModel {
+        let viewModel = AccountViewModel()
+        viewModel.userEmail = "test.email@testdomain.com"
+        viewModel.isLoggedIn = true
+        return viewModel
     }
 }
